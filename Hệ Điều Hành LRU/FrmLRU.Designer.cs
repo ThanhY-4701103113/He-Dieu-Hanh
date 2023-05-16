39508aac02e6f1e3962d274107b068161a3f4d4d
ThanhY-4701103113 <thanhyk20092003@gmail.com>
2023-05-16 13:27:48 +0700

    Soure Code

---
+++ Hệ Điều Hành LRU/FrmLRU.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Hệ_Điều_Hành_LRU
+{
+    partial class FrmLRU
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSoKhungTrang = new System.Windows.Forms.TextBox();
+            this.txtSoTrangAo = new System.Windows.Forms.TextBox();
+            this.txtChuoiTrang = new System.Windows.Forms.TextBox();
+            this.btnKiemTra = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pRun = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtThoiGian = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1532, 79);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(352, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(744, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CHIẾN LƯỢC THAY THẾ TRANG LRU";
+            // 
+            // txtSoKhungTrang
+            // 
+            this.txtSoKhungTrang.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoKhungTrang.Location = new System.Drawing.Point(267, 119);
+            this.txtSoKhungTrang.Name = "txtSoKhungTrang";
+            this.txtSoKhungTrang.Size = new System.Drawing.Size(92, 33);
+            this.txtSoKhungTrang.TabIndex = 1;
+            this.txtSoKhungTrang.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSoKhungTrang.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoTrang_KeyPress);
+            // 
+            // txtSoTrangAo
+            // 
+            this.txtSoTrangAo.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoTrangAo.Location = new System.Drawing.Point(267, 210);
+            this.txtSoTrangAo.Name = "txtSoTrangAo";
+            this.txtSoTrangAo.Size = new System.Drawing.Size(92, 33);
+            this.txtSoTrangAo.TabIndex = 2;
+            this.txtSoTrangAo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSoTrangAo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoTrangAo_KeyPress);
+            // 
+            // txtChuoiTrang
+            // 
+            this.txtChuoiTrang.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChuoiTrang.Location = new System.Drawing.Point(44, 352);
+            this.txtChuoiTrang.Name = "txtChuoiTrang";
+            this.txtChuoiTrang.Size = new System.Drawing.Size(315, 33);
+            this.txtChuoiTrang.TabIndex = 3;
+            // 
+            // btnKiemTra
+            // 
+            this.btnKiemTra.BackColor = System.Drawing.Color.Lime;
+            this.btnKiemTra.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnKiemTra.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKiemTra.Location = new System.Drawing.Point(41, 485);
+            this.btnKiemTra.Name = "btnKiemTra";
+            this.btnKiemTra.Size = new System.Drawing.Size(188, 56);
+            this.btnKiemTra.TabIndex = 5;
+            this.btnKiemTra.Text = "Kiểm tra";
+            this.btnKiemTra.UseVisualStyleBackColor = false;
+            this.btnKiemTra.Click += new System.EventHandler(this.btnKiemTra_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Số khung trang: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Số trang ảo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nhập chuỗi trang:";
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKetQua.Location = new System.Drawing.Point(41, 614);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(188, 33);
+            this.txtKetQua.TabIndex = 8;
+            this.txtKetQua.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 569);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Kết quả: ";
+            // 
+            // pRun
+            // 
+            this.pRun.BackColor = System.Drawing.Color.LightSlateGray;
+            this.pRun.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pRun.Location = new System.Drawing.Point(401, 79);
+            this.pRun.Name = "pRun";
+            this.pRun.Size = new System.Drawing.Size(1131, 674);
+            this.pRun.TabIndex = 10;
+            this.pRun.Paint += new System.Windows.Forms.PaintEventHandler(this.pRun_Paint);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(37, 430);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(172, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Thời gian chạy(s):";
+            // 
+            // txtThoiGian
+            // 
+            this.txtThoiGian.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtThoiGian.Location = new System.Drawing.Point(267, 427);
+            this.txtThoiGian.Name = "txtThoiGian";
+            this.txtThoiGian.Size = new System.Drawing.Size(92, 33);
+            this.txtThoiGian.TabIndex = 4;
+            this.txtThoiGian.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtThoiGian.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtThoiGian_KeyPress);
+            // 
+            // FrmLRU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1532, 753);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtThoiGian);
+            this.Controls.Add(this.pRun);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnKiemTra);
+            this.Controls.Add(this.txtChuoiTrang);
+            this.Controls.Add(this.txtSoTrangAo);
+            this.Controls.Add(this.txtSoKhungTrang);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmLRU";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mô phỏng hoạt động chiến lược thay thế trang LRU ";
+            this.Load += new System.EventHandler(this.FrmLRU_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSoKhungTrang;
+        private System.Windows.Forms.TextBox txtSoTrangAo;
+        private System.Windows.Forms.TextBox txtChuoiTrang;
+        private System.Windows.Forms.Button btnKiemTra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel pRun;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtThoiGian;
+    }
+}
+
---
+++ Hệ Điều Hành LRU/FrmLRU.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Hệ_Điều_Hành_LRU
+{
+    public partial class FrmLRU : Form
+    {
+        public static int tg = 0;
+        public FrmLRU()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSoTrang_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8) // 8 là phím backspace
+            {
+                e.Handled = true; // Không cho phép ký tự được nhập vào
+            }
+        }
+
+        private void txtSoTrangAo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8) // 8 là phím backspace
+            {
+                e.Handled = true; // Không cho phép ký tự được nhập vào
+            }
+        }
+
+        private void pRun_Paint(object sender, PaintEventArgs e)
+        {
+            int x = 50, y = 50, s = 40;
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            Font font = new Font("Times New Romen", 16);
+            SolidBrush br_MauTrang = new SolidBrush(Color.White);
+            SolidBrush br_MauXanhDuong = new SolidBrush(Color.Blue);
+            SolidBrush br_MauDen = new SolidBrush(Color.Black);
+            SolidBrush br_MauVang = new SolidBrush(Color.Yellow);
+            SolidBrush br_MauDo = new SolidBrush(Color.Red);
+            SolidBrush br_MauTim = new SolidBrush(Color.Purple);
+            List<SolidBrush> colorsbrush = new List<SolidBrush>() { new SolidBrush(Color.Red), new SolidBrush(Color.Blue), new SolidBrush(Color.Yellow), new SolidBrush(Color.Purple), new SolidBrush(Color.Green), new SolidBrush(Color.BurlyWood), new SolidBrush(Color.DarkCyan), new SolidBrush(Color.DarkOliveGreen), new SolidBrush(Color.DarkSlateBlue), new SolidBrush(Color.DarkKhaki), new SolidBrush(Color.CadetBlue), new SolidBrush(Color.Chocolate), new SolidBrush(Color.HotPink), new SolidBrush(Color.Indigo), new SolidBrush(Color.MediumSeaGreen), new SolidBrush(Color.PapayaWhip), new SolidBrush(Color.SkyBlue), new SolidBrush(Color.WhiteSmoke), new SolidBrush(Color.YellowGreen), new SolidBrush(Color.Tomato) };
+            List<Pen> colorspen = new List<Pen>() { new Pen(Color.Red, 3), new Pen(Color.Blue, 3), new Pen(Color.Yellow, 3), new Pen(Color.Purple, 3), new Pen(Color.Green), new Pen(Color.BurlyWood), new Pen(Color.DarkCyan), new Pen(Color.DarkOliveGreen), new Pen(Color.DarkSlateBlue), new Pen(Color.DarkKhaki) };
+            Pen pen_Black = new Pen(Color.White, 2);
+            Pen pen_Red = new Pen(Color.Red, 3);
+            Pen pen_Yellow = new Pen(Color.Yellow, 3);
+            Pen pen_Blue = new Pen(Color.Blue, 3);
+            string[] str;
+            str = txtChuoiTrang.Text.Split(new char[] { ' ' });
+            
+            List<string> Khung = new List<string>();
+            List<int> Dem = new List<int>();
+            if (txtSoTrangAo.Text != "" && txtSoKhungTrang.Text != "")
+            {
+                if (int.Parse(txtSoTrangAo.Text) > str.Length)
+                {
+                    return;
+                }
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] != "")
+                    {
+                        g.DrawLine(pen_Black, x, y, x + s, y);
+                        g.DrawLine(pen_Black, x, y, x, y + s);
+                        g.DrawLine(pen_Black, x + s, y, x + s, y + s);
+                        g.DrawLine(pen_Black, x, y + s, x + s, y + s);
+                        g.FillRectangle(br_MauXanhDuong, x + 2, y + 2, s - 4, s - 4);
+                        g.DrawString(str[i].ToString(), font, br_MauVang, x + 4, y + 5);
+                        x += s;
+                    }
+                }
+
+                x = 50;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    y = 3 * s;
+                    for (int i = 0; i < int.Parse(txtSoKhungTrang.Text); i++)
+                    {
+                        g.DrawLine(pen_Black, x, y, x + s, y);
+                        g.DrawLine(pen_Black, x, y, x, y + s);
+                        g.DrawLine(pen_Black, x + s, y, x + s, y + s);
+                        g.DrawLine(pen_Black, x, y + s, x + s, y + s);
+                        y += s;
+                    }
+                    x += s;
+                }
+                x = 50;
+                y = (int.Parse(txtSoKhungTrang.Text) + 4) * s;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    g.DrawLine(pen_Black, x, y, x + s, y);
+                    g.DrawLine(pen_Black, x, y, x, y + s);
+                    g.DrawLine(pen_Black, x + s, y, x + s, y + s);
+                    g.DrawLine(pen_Black, x, y + s, x + s, y + s);
+                    g.FillRectangle(br_MauTrang, x + 2, y+2, s - 4, s - 4);
+                    x += s;
+
+                }
+                int kq = 0;
+                y = 2*s;
+                x = 50;
+                List<string> Loi = new List<string>();
+                List<int> DanhDau = new List<int>();
+                for (int i = 0; i < str.Length; i++)
+                {
+                    string trang = str[i];
+                    int kt = 1000;
+                    if (!Khung.Contains(trang))
+                    {
+                        if (Khung.Count < int.Parse(txtSoKhungTrang.Text))
+                        {
+                            Khung.Add(trang);
+                            Dem.Add(i);
+                            DanhDau.Add(Khung.Count-1);
+                        }
+                        else
+                        {
+                            int minIndex = 0;
+                            for (int j = 1; j < int.Parse(txtSoKhungTrang.Text); j++)
+                            {
+                                if (Dem[j] < Dem[minIndex])
+                                {
+                                    minIndex = j;
+                                }
+                            }
+                            Khung[minIndex] = trang;
+                            Dem[minIndex] = i;
+                            DanhDau.Add(minIndex);
+                        }
+                        Loi.Add("F");
+                        kq++;
+                    }
+                    else
+                    {
+                        int index = Khung.IndexOf(trang);
+                        Dem[index] = i;
+                        DanhDau.Add(index);
+                        Loi.Add(" ");
+                    }
+                    if (i < int.Parse(txtSoKhungTrang.Text))
+                    {
+                        kt = int.Parse(txtSoKhungTrang.Text) + i;
+                    }
+                    int vt = y;
+                    g.FillRectangle(br_MauDo, x + 2, (DanhDau[i]+1)*s + vt + 2, s - 4, s - 4);
+                    for (int n = 0; n < Khung.Count; n++)
+                    {
+                        if (kt >= int.Parse(txtSoKhungTrang.Text))
+                        {
+                            //g.FillRectangle(br_MauDo, x + 2, vt + s, s - 2, s - 2);
+                            g.DrawString(Khung[n].ToString(), font, br_MauTrang, x + 4, vt + 5 + s);
+                            kt--;
+                        }
+                        vt += s;
+                        Thread.Sleep(tg*1600/str.Length/ int.Parse(txtSoKhungTrang.Text)/2);
+                    }
+                    g.DrawString(Loi[i], font, br_MauDo, x + 10, (int.Parse(txtSoKhungTrang.Text) + 4) * s + 5);
+                    Thread.Sleep(tg*1600/str.Length/ int.Parse(txtSoKhungTrang.Text)/2);
+                    x += s;
+                }
+                btnKiemTra.Enabled = true;
+                txtKetQua.Text = kq + " lỗi trang";
+                tg = 0;
+                btnKiemTra.Focus();
+            }
+        }
+
+        private void btnKiemTra_Click(object sender, EventArgs e)
+        {
+            if (txtSoTrangAo.Text == "")
+            {
+                txtSoTrangAo.Focus();
+                return;
+            }
+            if (txtSoKhungTrang.Text == "")
+            {
+                txtSoKhungTrang.Focus();
+                return;
+            }
+            btnKiemTra.Enabled = false;
+            if (txtThoiGian.Text == "")
+            {
+                tg = 0;
+            }
+            else
+            {
+                tg =int.Parse(txtThoiGian.Text);
+            }
+            pRun.Refresh();
+        }
+
+        private void txtThoiGian_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8) // 8 là phím backspace
+            {
+                e.Handled = true; // Không cho phép ký tự được nhập vào
+            }
+        }
+
+        private void FrmLRU_Load(object sender, EventArgs e)
+        {
+            txtKetQua.ReadOnly = true;
+        }
+    }
+}
